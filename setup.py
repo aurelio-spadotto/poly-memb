bb21aaac7bc4e3b38663a408dd76bae7baf8343d
aspadotto <aurelio-edoardo.spadotto@umontpellier.fr>
2026-02-25 10:27:35 +0100

    modif in setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,14 @@
 from setuptools import setup, find_packages
 
+# Read dependencies from requirements.txt
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
 setup(
     name="polymemb",
     version="0.1.0",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
-    install_requires=[
-        "meshio>=5.3,<6.0",
-    ],
+    install_requires=requirements,
 )
 
